7503480485021e895444da8f54db87cb2eacb4f9
hermesj <hermesj@uni-koeln.de>
2016-11-14 10:09:37 +0100

    First commit

---
+++ ml.toolclassification/src/main/java/de/uni_koeln/spinfo/ml/toolclassification/Application.java
@@ -0,0 +1,9 @@
+package de.uni_koeln.spinfo.ml.toolclassification;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Tool Classifying Application");
+	}
+
+}
