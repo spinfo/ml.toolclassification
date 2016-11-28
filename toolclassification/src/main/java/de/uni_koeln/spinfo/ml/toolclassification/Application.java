6fb4dcdeeee925e2b06c2e637ec55404e50f850b
hermesj <hermesj@philnet-pc0020.philnet.ad.uni-koeln.de>
2016-11-28 13:19:06 +0100

    Added crossvalidation

--- toolclassification/src/main/java/de/uni_koeln/spinfo/ml/toolclassification/Application.java
+++ toolclassification/src/main/java/de/uni_koeln/spinfo/ml/toolclassification/Application.java
@@ -1,8 +1,13 @@
 package de.uni_koeln.spinfo.ml.toolclassification;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import de.uni_koeln.spinfo.ml.toolclassification.components.DataImporter;
+import de.uni_koeln.spinfo.ml.toolclassification.components.crossvalidation.CrossvalidationGroupBuilder;
+import de.uni_koeln.spinfo.ml.toolclassification.components.crossvalidation.TrainingTestSets;
+import de.uni_koeln.spinfo.ml.toolclassification.data.Tool;
 
 public class Application {
 
@@ -11,8 +16,21 @@ public class Application {
 		System.out.println("----------------------------");
 		DataImporter di = new DataImporter();
 		di.parseToolsAndClassesFromFile("src/main/resources/data/DatenTools.tsv");
+//		
+//		Set<String> keySet = di.getTools().keySet();
+//		for (String string : keySet) {
+//			System.out.println(di.getTools().get(string).getParentClassId());
+//		}
 		
-		System.out.println(di.getTools().size());
+		List<Tool> values = new ArrayList<Tool>(di.getTools().values());
+		CrossvalidationGroupBuilder<Tool> cvgb = new CrossvalidationGroupBuilder<Tool>(values, 10);
+		for (TrainingTestSets<Tool> tts : cvgb) {
+			System.out.println(tts.getTrainingSet().size()); //--> Build model / choose classifier
+			System.out.println(tts.getTestSet().size()); //--> Classify 
+			//--> Evaluate
+			System.out.println();
+		}
+		//--> Calculate mean
 	}
 
 }
